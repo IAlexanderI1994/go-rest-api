2e247f036b728d0b1d6e9a1e2d218a850b883ccd
blaze <kontaktAK@Yandex.ru>
2019-08-15 19:55:27 +0300

    mux router Book/Author models

---
+++ index.go
@@ -0,0 +1,32 @@
+package go_rest_api
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+// Book Model
+
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
