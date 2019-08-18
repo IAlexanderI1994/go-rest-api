80355bba307483e143f56a521ab22afd0623978d
blaze <kontaktAK@Yandex.ru>
2019-08-18 15:49:44 +0300

    all books/single book routes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
-package go_rest_api
+package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -9,18 +10,49 @@ import (
 // Book Model
 
 type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
+	ID     string `json:"id"`
+	Isbn   string `json:"isbn"`
+	Title  string `json:"title"`
+	Author Author `json:"author"`
 }
 type Author struct {
 	Firstname string `json:"firstname"`
 	Lastname  string `json:"lastname"`
 }
 
+var books []Book
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+
+}
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(Book{})
+}
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
 func main() {
 	r := mux.NewRouter()
+
+	books = append(books, Book{"1", "44873", "Book one", Author{"John", "Doe"}})
+	books = append(books, Book{"2", "45486", "Book two", Author{"Steve", "Smith"}})
+
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
